db3959824af4d082101bb76ea040aa7326f93377
Rubi <rubihau2010@gmail.com>
2018-11-19 12:54:19 -0500

    Prueba
    Miren

--- views/site/index.php
+++ views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Hola Mundo</h1>
+        <h1>Hola Compañeros</h1>
 
-        <p class="lead">You have successfully created your jajajajajja.</p>
+        <p class="lead">¿Que tal? jajajajajja.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
